048ad26e3fc90de6a7c93b4157323d171b33e3be
Oluwaseun-D'Silva <106748698+Silva-6@users.noreply.github.com>
2022-09-20 06:14:14 +0100

    Update 10-linked_lists.c

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -9,19 +7,19 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
 
-    return (n);
+	return (n);
 }
 
 /**
@@ -32,17 +30,17 @@ size_t print_listint(const listint_t *h)
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-    return (new);
+	return (new);
 }
 
 /**
@@ -52,12 +50,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *current;
 
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
 }
