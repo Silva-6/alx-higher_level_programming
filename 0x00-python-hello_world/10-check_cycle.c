e58884d99f833f21ee1bba2b4f152d28db59e053
Oluwaseun-D'Silva <106748698+Silva-6@users.noreply.github.com>
2022-09-20 06:01:07 +0100

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,21 @@
 #include "lists.h"
 
-/**
- * check_cycle - Write a function in C that checks if 
- * a singly linked list has a cycle in it.
- *
- * @list: The pointer to the linked list
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle
- */
 int check_cycle(listint_t *list)
 {
-	listint_t *currentS = list;
-	listint_t *currentF = list;
+	listint_t *tortoise, *hare;
 
-	while(currentS && currentF && currentF->next)
+	tortoise = list;
+	if (!list)
+		return (0);
+
+	hare = list->next;
+
+	while (hare && hare->next)
 	{
-		currentS = currentS->next;
-		currentF = currentF->next->next;
-		if (currentF == currentS)
+		if (tortoise == hare)
 			return (1);
+		hare = hare->next->next;
+		tortoise = tortoise->next;
 	}
-
 	return (0);
 }
