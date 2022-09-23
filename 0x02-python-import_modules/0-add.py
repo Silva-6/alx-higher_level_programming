6e6cef0f039099196b80dd75b37bb3ff563f5730
Silva-6 <silva6media@gmail.com>
2022-09-23 04:40:58 +0000

    fixed some errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__=="__main__":
     from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    _sum = add(a, b)
+    print("{} + {} = {}".format(a, b, _sum))
