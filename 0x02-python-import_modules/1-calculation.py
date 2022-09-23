6fe7955e3db866377a89beb3c1e79ab3e9eb9194
Silva-6 <silva6media@gmail.com>
2022-09-23 03:38:04 +0000

    python import modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import {
+        add,
+        sub,
+        mul,
+        div
+        }
+
+a = 10
+b = 5
+print("{} + {} = {}".format(add(a, b)))
+print("{} - {} = {}".format(sub(a, b)))
+print("{} * {} = {}".format(mul(a, b)))
+print("{} + /{} = {}".format(div(a, b)))
