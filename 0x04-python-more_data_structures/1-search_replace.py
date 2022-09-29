5e20ee518cb09470bb22ef4bc54baa7d2ef17d53
Silva-6 <silva6media@gmail.com>
2022-09-29 01:16:47 +0000

    Adjusted

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i in new_list:
         if i == search:
-            new_list[new_list.index(i)]
+            new_list[new_list.index(i)] = replace
     return new_list
