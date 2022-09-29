108d09494f5038e3f4cb9708444ee2228226de34
Silva-6 <silva6media@gmail.com>
2022-09-29 00:51:50 +0000

    1-search_replace.py made executable

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,3 +7,9 @@ def search_replace(my_list, search, replace):
         if new_list[i] == search:
             new_list[i] = replace
     return new_list
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
